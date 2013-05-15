042b8b81448ba28d4ea1adf467c474fa2a724775
Terry Tice <nufyoot@gmail.com>
2013-05-15 00:06:41 -0400

    Added config, tests, and file checks

--- test/main.js
+++ test/main.js
@@ -1,7 +1,9 @@
 var chai = require('chai');
 var KallyRazor = require('../lib/main');
 
-var razor = KallyRazor();
+var razor = KallyRazor({
+    root: __dirname
+});
 
 var should = chai.should();
 
@@ -14,12 +16,23 @@ describe('KallyRazor', function() {
         it('when not passing a string to render', function() {
             should.Throw(function() { razor.render({ File: 'test' }); });
         });
+
+        it('when a file does not exist', function() {
+            should.Throw(function() { razor.render('file-does-not-exist.html'); });
+        });
     });
 
     describe('compiles', function() {
-        it('from a file', function() {
-            var result = razor.render('view', 'blah');
-            result.should.equal('blah');
+        describe('from a file', function() {
+            it('using the exact file name', function() {
+                var result = razor.render(__dirname + '/input/test-no-razor.html');
+                result.should.equal('it exists');
+            });
+
+            it('using relative file names', function() {
+                var result = razor.render('input/test-no-razor.html');
+                result.should.equal('it exists');
+            });
         });
     });
 });
