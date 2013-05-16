3135f836506f642742681d29e56b6249fbfdb104
Terry Tice <nufyoot@gmail.com>
2013-05-16 08:45:57 -0400

    Time to use coveralls

--- test/main.js
+++ test/main.js
@@ -1,5 +1,6 @@
+var libPath = process.env['KALLY_RAZOR_COV'] ? '../lib-cov' : '../lib';
 var chai = require('chai');
-var KallyRazor = require('../lib/main');
+var KallyRazor = require(libPath + '/main');
 var fs = require('fs');
 
 var razor = KallyRazor({
