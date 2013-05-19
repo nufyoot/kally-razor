49acecf1c76a34d526aab5c8dade68f986118645
Terry Tice <nufyoot@gmail.com>
2013-05-19 15:58:09 -0400

    Tuned the parsing of a smiple razor statement 1,000,000 times from 2.5 seconds to 1.5 seconds

---
+++ benchmarks/parsing.js
@@ -0,0 +1,15 @@
+var KallyRazor = require('../lib/main');
+var Parser = require('../lib/parser');
+
+var razor = KallyRazor();
+var parser = Parser();
+var start;
+var contents;
+var i;
+
+start = Date.now();
+contents = "This is a very small test @model.name";
+for (i = 0; i < 1000000; i++) {
+    parser.parseRazorContents(contents);
+}
+console.log('Parsing razor token ' + i + ' times in ' + (Date.now() - start) + ' ms');
