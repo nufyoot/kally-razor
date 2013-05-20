383a9675e2c18817e8cff02357a2c97245134d2d
Terry Tice <nufyoot@gmail.com>
2013-05-19 20:44:05 -0400

    Starting to add support for templating.

---
+++ test/templates.js
@@ -0,0 +1,20 @@
+var libPath = process.env['KALLY_RAZOR_COV'] ? '../lib-cov/' : '../lib/';
+var chai = require('chai');
+var KallyRazor = require(libPath + 'main');
+var Parser = require(libPath + 'parser');
+var fs = require('fs');
+
+var should = chai.should();
+
+describe('Templating', function() {
+    describe('compiles', function() {
+        it('with basic layout', function() {
+            var razor = KallyRazor({
+                root: __dirname,
+                layout: "input/_test-basic-layout.html"
+            });
+            var result = razor.render('input/test-basic-layout.html');
+            result.should.equal(fs.readFileSync(__dirname + '/output/test-basic-layout.html').toString());
+        })
+    });
+});
